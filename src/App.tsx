8c5ca52aa1ab931743312ce9bdffddc4e47efb2c
Jonathan Ortega <jortega0389@gmail.com>
2024-03-14 15:52:43 -0500

    feat: finish joke list with pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import JokesList from "./components/JokesList";
 import Pagination from "./components/Pagination";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState(1);
   return (
-    <div className="flex items-center flex-col w-full">
+    <div className="flex items-center flex-col w-full py-5">
+      <h1 className="font-sans mb-8 text-4xl">Jokes directory</h1>
       <JokesList />
-      <Pagination className="mt-12" />
+      <Pagination
+        className="mt-12"
+        currentPage={currentPage}
+        total={5}
+        limit={10}
+        onPaginationChange={(page) => setCurrentPage(page)}
+      />
     </div>
   );
 }
--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -4,28 +4,45 @@ export type PaginationProps = {
   total: number;
   limit: number;
   currentPage: number;
+  onPaginationChange: (page: number) => void;
 } & HTMLAttributes<HTMLDivElement>;
 
-function paginationBuilder(total, limit) {
-  return {
-    totalPages: Math.ceil(total / limit),
-    limit,
-  };
+function getTotalPages(total: number, limit: number) {
+  return total > limit ? Math.ceil(total / limit) : 1;
 }
 
 export default function Pagination({
   total,
   limit,
   currentPage,
-  onChange,
+  onPaginationChange,
   className,
 }: PaginationProps) {
-  const content = paginationBuilder(total, limit);
+  const totalPages = getTotalPages(total, limit);
+  console.log({ totalPages });
+  const canGoPrev = currentPage !== 1;
+  const canGoNext = currentPage < totalPages;
   return (
-    <div className={`flex ${className} font-sans gap-3`}>
-      <a className="text-bold no-underline" href="#">Prev</a>
-      <a className="text-bold no-underline" href="#">1</a>
-      <a className="text-bold no-underline" href="#">next</a>
+    <div
+      className={`flex ${className} font-sans gap-3 bg-white rounded-md items-center`}
+    >
+      <button
+        className="inline-flex py-2 px-3 border-4 border-transparent rounded-l-md font-serif ease-in-out hover:border-t-purple-500 transition-colors disabled:bg-slate-100 disabled:border-t-transparent"
+        disabled={!canGoPrev}
+        onClick={() => onPaginationChange(currentPage - 1)}
+      >
+        Prev
+      </button>
+      <p className="inline-flex no-underline p-2">
+        {currentPage} / {totalPages}
+      </p>
+      <button
+        className="inline-flex py-2 px-3 border-4 border-transparent font-serif rounded-r-md ease-in-out hover:border-t-purple-500 transition-colors disabled:bg-slate-100 disabled:border-t-transparent"
+        disabled={!canGoNext}
+        onClick={() => onPaginationChange(currentPage + 1)}
+      >
+        Next
+      </button>
     </div>
   );
 }
